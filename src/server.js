767467df77ecafa268aa24b0cd45c9faebb0e2d0
Alena <shining8smaragd@gmail.com>
2026-07-29 13:07:39 +0300

    fix: correct the routes

--- src/server.js
+++ src/server.js
@@ -26,6 +26,12 @@ app.use(
   }),
 );
 
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Server is running',
+  });
+});
+
 app.get('/notes', (req, res) => {
   res.status(200).json({
     message: 'Retrieved all notes',
@@ -33,7 +39,7 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = Number(req.params.noteId);
+  const { noteId } = req.params;
   res.status(200).json({
     message: `Retrieved note with ID: ${noteId}`,
   });
