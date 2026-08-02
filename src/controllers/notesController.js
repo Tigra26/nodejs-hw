9b3ded96e3cdb9e7043e162148b9d2f30573195a
Alena <shining8smaragd@gmail.com>
2026-08-02 11:03:10 +0300

    feat: add notes model, routes and controllers

---
+++ src/controllers/notesController.js
@@ -0,0 +1,18 @@
+import createHttpError from 'http-errors';
+import { Note } from '../models/note.js';
+
+export const getNotes = async (req, res) => {
+  const notes = await Note.find();
+  res.status(200).json(notes);
+};
+
+export const getNoteById = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findById(noteId);
+
+  if (!note) {
+    throw createHttpError(404, `Cannot find note with id=${noteId}`);
+  }
+
+  res.status(200).json(note);
+};
